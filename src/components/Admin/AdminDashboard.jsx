8ace5889547fd9105b3e7f275d5378a45eea18dc
archways <107893165+archways404@users.noreply.github.com>
2023-11-30 23:40:46 +0100

    fix

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -33,7 +33,7 @@ export default function AdminDashboard() {
 	useEffect(() => {
 		const fetchMessages = async () => {
 			try {
-				const response = await fetch('https://82.183.49.2:9999/api/messages');
+				const response = await fetch('http://localhost:9999/api/messages');
 				if (!response.ok) {
 					throw new Error(`HTTP error! status: ${response.status}`);
 				}
@@ -63,7 +63,7 @@ export default function AdminDashboard() {
 	const deleteMessageByTimestamp = async (timestamp) => {
 		try {
 			console.log('timestamp: ', timestamp);
-			const response = await fetch('https://82.183.49.2:9999/api/messages', {
+			const response = await fetch('http://localhost:9999/api/messages', {
 				method: 'DELETE',
 				headers: {
 					'Content-Type': 'application/json',
@@ -97,7 +97,7 @@ export default function AdminDashboard() {
 		e.preventDefault();
 
 		try {
-			const response = await fetch('https://82.183.49.2:9999/api/messages', {
+			const response = await fetch('http://localhost:9999/api/messages', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
