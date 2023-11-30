4115c68cbf4b0305637b1ca76d65db3bd41aee0a
archways <107893165+archways404@users.noreply.github.com>
2023-11-30 12:07:37 +0100

    dix

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<BrowserRouter>
 			<Routes>
 				<Route
-					path="/archways/"
+					path="/"
 					element={<Welcome />}
 				/>
 			</Routes>
