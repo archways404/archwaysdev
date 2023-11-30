e2121b53994e8bedce1ff64f6f0cb0eb3474b966
archways <107893165+archways404@users.noreply.github.com>
2023-11-30 23:05:50 +0100

    updated

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -129,7 +129,7 @@ export default function AdminDashboard() {
 	};
 
 	return (
-		<section className="min-h-screen bg-primary flex flex-col items-center justify-center">
+		<section className="min-h-screen bg-primary flex flex-col items-center justify-center p-4">
 			<h1 className="text-6xl font-bold text-yellow-500 mb-6">404</h1>
 			<ToastContainer
 				toastClassName={({ type }) =>
@@ -140,107 +140,115 @@ export default function AdminDashboard() {
 				position="bottom-left"
 				autoClose={3000}
 			/>
-			<div>
-				{Object.entries(messages).map(([key, message]) => (
-					<div
-						key={key}
-						className="bg-white shadow-md rounded p-4 mb-4">
-						<div className="mb-2">
-							<label
-								htmlFor={`name-${key}`}
-								className="font-bold mr-2">
-								Name:
-							</label>
-							<input
-								type="text"
-								id={`name-${key}`}
-								className="bg-gray-100 p-2 rounded border border-gray-300"
-								value={key}
-								disabled
-							/>
-						</div>
-
-						<div className="mb-2">
-							<label
-								htmlFor={`message-${key}`}
-								className="font-bold mr-2">
-								Message:
-							</label>
-							<input
-								type="text"
-								id={`message-${key}`}
-								className="bg-gray-100 p-2 rounded border border-gray-300"
-								value={message.text}
-								disabled
-							/>
-						</div>
-
-						<div className="mb-2">
-							<label
-								htmlFor={`color-${key}`}
-								className="font-bold mr-2">
-								Color:
-							</label>
-							<input
-								type="text"
-								id={`color-${key}`}
-								className="bg-gray-100 p-2 rounded border border-gray-300"
-								value={message.color}
-								disabled
-							/>
-						</div>
-
-						<div>
-							<label
-								htmlFor={`timestamp-${key}`}
-								className="font-bold mr-2">
-								Timestamp:
-							</label>
-							<input
-								type="text"
-								id={`timestamp-${key}`}
-								className="bg-gray-100 p-2 rounded border border-gray-300"
-								value={new Date(message.timestamp).toLocaleString()}
-								disabled
-							/>
-							<br></br>
+			<div className="flex flex-col md:flex-row w-full max-w-7xl">
+				{/* Messages Section */}
+				<div className="flex-1 md:mr-4">
+					{Object.entries(messages).map(([key, message]) => (
+						<div
+							key={key}
+							className="bg-primary text-white p-4 rounded border mb-4">
+							<div className="grid grid-cols-2 gap-4">
+								<div>
+									<label
+										htmlFor={`name-${key}`}
+										className="font-bold mr-2">
+										Name:
+									</label>
+									<input
+										type="text"
+										id={`name-${key}`}
+										className="bg-gray-700 p-2 rounded border border-gray-600 w-full"
+										value={key}
+										disabled
+									/>
+								</div>
+								<div>
+									<label
+										htmlFor={`color-${key}`}
+										className="font-bold mr-2">
+										Color:
+									</label>
+									<input
+										type="text"
+										id={`color-${key}`}
+										className="bg-gray-700 p-2 rounded border border-gray-600 w-full"
+										value={message.color}
+										disabled
+									/>
+								</div>
+								<div>
+									<label
+										htmlFor={`timestamp-${key}`}
+										className="font-bold mr-2">
+										Timestamp:
+									</label>
+									<input
+										type="text"
+										id={`timestamp-${key}`}
+										className="bg-gray-700 p-2 rounded border border-gray-600 w-full"
+										value={new Date(message.timestamp).toLocaleString()}
+										disabled
+									/>
+								</div>
+								<div>
+									<label
+										htmlFor={`message-${key}`}
+										className="font-bold mr-2">
+										Message:
+									</label>
+									<textarea
+										type="text"
+										id={`message-${key}`}
+										className="bg-gray-700 p-2 rounded border border-gray-600 w-full"
+										value={message.text}
+										disabled
+									/>
+								</div>
+							</div>
+							<div className="mb-2">{/* ... */}</div>
 							<button
 								onClick={() => deleteMessageByTimestamp(message.timestamp)}
-								className="bg-red-500 text-white p-2 rounded mt-2 hover:bg-red-600">
+								className="bg-primary border text-white p-2 rounded mt-2 hover:bg-red-800 hover:border-transparent">
 								DELETE
 							</button>
 						</div>
-					</div>
-				))}
-				<form onSubmit={handleSubmit}>
-					<input
-						type="text"
-						value={newName}
-						onChange={(e) => setNewName(e.target.value)}
-						placeholder="Enter name"
-					/>
-					<br></br>
-					<br></br>
-					<textarea
-						type="text"
-						value={newMessage}
-						onChange={(e) => setNewMessage(e.target.value)}
-						placeholder="Enter message"
-					/>
-					<br></br>
-					<br></br>
-					<select
-						value={color}
-						onChange={(e) => setColor(e.target.value)}>
-						<option value="">Select color</option>
-						<option value="red">Red</option>
-						<option value="blue">Blue</option>
-						<option value="green">Green</option>
-					</select>
-					<br></br>
-					<br></br>
-					<button type="submit">Create Message</button>
-				</form>
+					))}
+				</div>
+
+				{/* Form Section */}
+				<div className="md:w-1/3 md:ml-4 mt-4 md:mt-0">
+					<form
+						onSubmit={handleSubmit}
+						className="bg-primary text-white p-4  rounded">
+						<input
+							type="text"
+							value={newName}
+							onChange={(e) => setNewName(e.target.value)}
+							placeholder="Enter name"
+							className="p-2 rounded border border-gray-600 bg-gray-700 w-full mb-4"
+						/>
+						<textarea
+							value={newMessage}
+							onChange={(e) => setNewMessage(e.target.value)}
+							placeholder="Enter message"
+							className="p-2 rounded border border-gray-600 bg-gray-700 w-full mb-4"
+						/>
+						<select
+							value={color}
+							onChange={(e) => setColor(e.target.value)}
+							className="p-2 rounded border border-gray-600 bg-gray-700 w-full mb-4">
+							<option value="">Select color</option>
+							<option value="red">Red</option>
+							<option value="yellow">Yellow</option>
+							<option value="green">Green</option>
+						</select>
+						<button
+							type="submit"
+							className="bg-primary border hover:bg-green-700 hover:border-transparent text-white font-bold py-2 px-4 rounded w-full">
+							Create Message
+						</button>
+					</form>
+				</div>
 			</div>
 		</section>
 	);
