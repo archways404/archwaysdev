8d244bfddbb1c82b5f4acdb3fcad8891c9ac0aa2
archways <107893165+archways404@users.noreply.github.com>
2023-11-30 12:52:35 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<Routes>
 				<Route
 					path="/"
 					element={<Welcome />}
-				/>
+        />
 			</Routes>
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 );
