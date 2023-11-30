8d304ddb4212508ef2d20c177ad292c79b22ee8a
archways <107893165+archways404@users.noreply.github.com>
2023-11-30 23:25:23 +0100

    f

--- src/components/Welcome/ConstructionBeta.jsx
+++ src/components/Welcome/ConstructionBeta.jsx
@@ -1,10 +1,48 @@
+import { useState, useEffect } from 'react';
+
 export default function ConstructionBeta() {
+	const [statusMessage, setStatusMessage] = useState(null);
+
+	useEffect(() => {
+		const fetchMessages = async () => {
+			try {
+				const response = await fetch('http://localhost:9999/api/messages');
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+				const data = await response.json();
+				setStatusMessage(data.Messages);
+				console.log(data.Messages);
+			} catch (error) {
+				console.error('Failed to fetch messages:', error);
+			}
+		};
+
+		fetchMessages();
+	}, []);
+
 	return (
 		<section className="min-h-screen bg-primary flex flex-col items-center justify-center">
+			{statusMessage && (
+				<div className="w-full fixed top-0 z-50">
+					{Object.entries(statusMessage).map(([key, message]) => (
+						<div
+							key={key}
+							style={{ backgroundColor: message.color }}
+							className="p-2 text-black rounded-t mb-4 text-center">
+							<p>{message.text}</p>
+							<p className="text-gray-400 text-bold">
+								Last updated: {new Date(message.timestamp).toLocaleString()}
+							</p>
+						</div>
+					))}
+				</div>
+			)}
 			<h1 className="text-6xl font-bold text-gray-400 mb-6">archways404.dev</h1>
 			<h1 className="text-4xl font-bold text-yellow-300 mb-12">
 				is currently under construction
 			</h1>
+
 			<div className="animate-bounce text-3xl mb-8">
 				<IconConstruction className="h-12 w-12 text-yellow-500" />
 			</div>
