3ecae6cdf641df7f90cd3e96df1cecfc4dc37a37
archways <107893165+archways404@users.noreply.github.com>
2023-12-04 08:42:58 +0100

    fix

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -33,7 +33,7 @@ export default function AdminDashboard() {
 	useEffect(() => {
 		const fetchMessages = async () => {
 			try {
-				const response = await fetch('https://82.183.49.2:9999/api/messages');
+				const response = await fetch('https://localhost:9999/api/messages');
 				if (!response.ok) {
 					throw new Error(`HTTP error! status: ${response.status}`);
 				}
